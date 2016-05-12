b5800d1091422a610d5b6b1d923816257b9cbdcd
iamgs7 <gs7.civic@gmail.com>
2016-05-12 03:49:52 -0700

    Complete Restructuring of Code
    --Committed by iamgs7

--- config-template.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    yelp: {
-        "consumer_key": "",
-        "consumer_secret": "",
-        "token": "",
-        "token_secret": "",
-    }
-}
--- server.js
+++ server.js
@@ -1,139 +1,50 @@
-"use strict";
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var path = require('path');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var configDB = require('./config/database');
 
-var express = require("express"),
-    bodyParser = require("body-parser"),
-    http = require("http"),
-    Yelp = require("yelp"),
-    mongoose = require("mongoose"),
-    app = express(),
-    configAuth = require('./config');
+app.use(express.static(path.join(__dirname, 'public')));
+require('./config/passport')(passport); // pass passport for configuration
 
-//// VAR FOR IMAGE UPLOAD IN REVIEW ////////
-var multer = require('multer'),
-    file_name,
-    final_filepath;
-
-
-var yelp = new Yelp(configAuth.yelp);
-
-app.use(express.static("."));
-app.use(bodyParser.json({
-    type: 'application/*+json'
-}));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-
-//////// MONGODB CONNECTION //////////////
-mongoose.connect("mongodb://admin:admin@ds015942.mlab.com:15942/foodnearme", function(error, success) {
+// configuration ===============================================================
+mongoose.connect(configDB.url, function(error, success) {
     if (error) {
         console.log("Error connecting to database: \n" + error);
     } else {
         console.log('Connected to Database');
     }
-});
+}); // connect to our database
 
-var reviewSchema = mongoose.Schema({
-    restaurant: String,
-    rating: Number,
-    written: String,
-    restaurantID: String,
-    date: String,
-    image: String
-});
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-var reviews = mongoose.model("reviews", reviewSchema);
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-///////// IMAGE UPLOAD FUNCTIONS ///////
-
-var storage = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null, './uploads');
-
-    },
-    filename: function(req, file, callback) {
-        var file_type = file.mimetype.split("/")[1];
-        file_name = file.fieldname + '-' + Date.now() + '.' + file_type;
-        callback(null, file_name);
-        final_filepath = "/uploads/" + file_name;
-    }
-});
-
-var upload = multer({
-    storage: storage
-}).single('userphoto');
-
-//////// IMAGE FUNCTIONS DONE //////////
+// required for passport
+app.use(session({
+    secret: 'thisiscodedbygs7'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 
-////////// ROUTES ///////////////
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-app.get('/find', function(req, res) {
-    console.log("Latitude = " + req.query.latitude);
-    console.log("Longitude = " + req.query.longitude);
 
-    var swLat = parseFloat(req.query.latitude) - 0.0075;
-    var swLong = parseFloat(req.query.longitude) - 0.0075;
-    var neLat = parseFloat(req.query.latitude) + 0.0075;
-    var neLong = parseFloat(req.query.longitude) + 0.0075;
-
-    yelp.search({
-        term: "food",
-        bounds: swLat + "," + swLong + "|" + neLat + "," + neLong
-    })
-        .then(function(data) {
-            res.json(data);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-});
-
-app.post('/review', function(req, res) {
-    var tempReview = new reviews({
-        restaurant: req.body.restaurant,
-        rating: req.body.rating,
-        written: req.body.written,
-        restaurantID: req.body.restaurantID,
-        date: req.body.date,
-        image: req.body.image
-    });
-    console.log(tempReview);
-
-    tempReview.save(function(err, tempReview) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Review Successfully Saved!");
-        res.json(tempReview);
-    });
-});
-
-app.get('/review', function(req, res) {
-    reviews.find({
-        restaurant: req.query.restaurant
-    }, function(err, obj) {
-        if (err) {
-            return console.log(err);
-        }
-        res.json(obj);
-    });
-});
-
-app.post('/api/photo', function(req, res) {
-    upload(req, res, function(err) {
-        if (err) {
-            return res.end("Error uploading file.");
-        }
-        res.send(final_filepath);
-    });
-});
-
-//////// SERVER CONNECTION ///////////
-http.createServer(app).listen(3000, function() {
-    console.log('Server listening on port 3000');
-});
+app.listen(port, function(){
+    console.log("Go find your food at http://localhost:"+port);
+})
